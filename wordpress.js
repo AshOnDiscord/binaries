0b3cc5df5ee420bf0b41fc0677bb028b65bf9360
Ash <68016221+AshOnDiscord@users.noreply.github.com>
2024-06-23 23:18:18 -0400

    Update wordpress.js

--- wordpress.js
+++ wordpress.js
@@ -1,11 +1,11 @@
 const mapElement = document.createElement("div");
 const hideMap = () => {
-mapElement.style.display = "none";
-mapElement.style.opacity = "0";
+  mapElement.style.display = "none";
+  mapElement.style.opacity = "0";
 };
 const showMap = () => {
-mapElement.style.display = "block";
-mapElement.style.opacity = "1";
+  mapElement.style.display = "block";
+  mapElement.style.opacity = "1";
 };
 mapElement.id = "map";
 mapElement.style.width = "100vw";
@@ -16,24 +16,26 @@ mapElement.style.left = "0";
 mapElement.style.zIndex = "1000";
 mapElement.style.animation = "fade-in-out 0.5s";
 
-document.body.appendChild(mapElement)
+document.body.appendChild(mapElement);
 
 const markerClick = async (e) => {
-    console.log(`Marker ${e.target.options.name} clicked`, e);
+  console.log(`Marker ${e.target.options.name} clicked`, e);
 
-    document
-      .querySelector(`#${e.target.options.htmlID}`)
-      ?.scrollIntoView({ behavior: "smooth" });
+  document
+    .querySelector(`#${e.target.options.htmlID}`)
+    ?.scrollIntoView({ behavior: "smooth" });
 };
 
 const map = leaflet.map("map").setView({ lat: 40.72438, lng: -74.305332 }, 18);
-console.log(map)
-console.log(document.querySelector("#map"))
-leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
-  maxZoom: 19,
-}).addTo(map);
+console.log(map);
+console.log(document.querySelector("#map"));
+leaflet
+  .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+    maxZoom: 19,
+  })
+  .addTo(map);
 
 map.addEventListener("click", (e) => {
   console.log(e.latlng);
@@ -51,13 +53,96 @@ document.querySelectorAll("[data-map-count]").forEach((element) => {
     html: `<div id="${elementName}MarkerIcon" class="markerIconInner">${count}</div>`,
   });
 
-  const marker = leaflet.marker({ lat, lng }, {
-    icon,
-    name,
-    htmlID: elementName,
-    count
-  }).addTo(map);
+  const marker = leaflet
+    .marker(
+      { lat, lng },
+      {
+        icon,
+        name,
+        htmlID: elementName,
+        count,
+      },
+    )
+    .addTo(map);
   marker.addEventListener("click", markerClick);
+});
+
+document.addEventListener("scroll", (e) => {
+  const mapHeight = document.querySelector("#map").clientHeight;
+
+  const percentages = [...document.querySelectorAll(".markerBox")].map(
+    (element) => {
+      // get how much the element is visible in the viewport, remember the map covers the top 50vh
+      const rect = element.getBoundingClientRect();
+
+      // console.log(rect.top - mapHeight, rect.bottom - mapHeight, window.innerHeight, window.innerHeight - mapHeight, element.id);
+
+      if (rect.top > window.innerHeight || rect.bottom < mapHeight) {
+        // below and above the viewport
+        return {
+          id: element.id,
+          percentage: -1, // below the viewport
+        };
+      }
+
+      const topPos = Math.min(
+        rect.bottom - mapHeight,
+        window.innerHeight - mapHeight,
+      );
+      const bottomPos = Math.max(
+        0,
+        rect.top - (window.innerHeight - mapHeight),
+      );
+
+      return {
+        id: element.id,
+        percentage:
+          ((topPos - bottomPos) / (window.innerHeight - mapHeight)) * 100,
+      };
+    },
+  );
+
+  // console.log(percentages.map(JSON.stringify).join("\n"));
+
+  let largestElement = null;
+  for (const element of percentages) {
+    if (element.percentage === -1) {
+      continue;
+    }
+
+    if (
+      largestElement === null ||
+      element.percentage > largestElement.percentage
+    ) {
+      largestElement = element;
+      // console.log("New largest element", element);
+    }
+  }
+
+  if (largestElement !== null) {
+    // console.log(`Largest element is ${largestElement.id} with ${largestElement.percentage}`, currentMarker);
+    if (largestElement.id !== currentMarker) {
+      console.log("Scrolling to", largestElement.id);
+      const marker = document.querySelector(`#${largestElement.id}`);
+
+      const htmlElement = document.getElementById(largestElement.id);
+      const lat = parseFloat(htmlElement.getAttribute("data-map-lat"));
+      const lng = parseFloat(htmlElement.getAttribute("data-map-lng"));
+
+      map.flyTo({ lat, lng }, 19, {
+        animate: true,
+        duration: 0.25,
+        easeLinearity: 0.5,
+      });
+      document
+        .getElementById(`${currentMarker}MarkerIcon`)
+        ?.classList.remove("markerIconActive");
+      currentMarker = largestElement.id;
+      document
+        .getElementById(`${currentMarker}MarkerIcon`)
+        ?.classList.add("markerIconActive");
+    }
+  }
 });
 
 hideMap();
